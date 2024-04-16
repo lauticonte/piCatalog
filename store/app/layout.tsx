028fbca37eebc266d35c8c2a66b5a7ba0d3b1a1d
Lautaro Conte <info@contelautaro.com.ar>
2024-04-16 15:46:56 -0300

    🚧 metadata

--- store/app/layout.tsx
+++ store/app/layout.tsx
@@ -12,11 +12,25 @@ import ReactQueryProvider from '@/providers/react-query-provider'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'mhGarage',
-  description: 'mhGarage',
+  title: 'MH Garage - Herramientas',
+  description: 'Catálogo de herramientas online. Consultas por WhatsApp. Envíos a todo el país.',
   icons: {
     icon: '/logo.png',
   },
+  openGraph: {
+    type: 'website',
+    locale: 'es_AR',
+    url: 'https://mhgarage.ar/',
+    title: 'MH Garage - Herramientas',
+    description: 'Catálogo de herramientas online. Consultas por WhatsApp. Envíos a todo el país.',
+    images: [
+      {
+        url: '/banner.png',
+        width: 800,
+        height: 600,
+        alt: 'MH Garage - Herramientas',
+      },
+    ]},
   authors: {
     name: 'Lautaro Conte',
     url: 'https://contelautaro.com.ar/',
